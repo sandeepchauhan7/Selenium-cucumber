1e8458302cc314fa5104ac1a8ba04a7730e33ba1
Sandeep Chauhan <sandeep.chauhan.mail@gmail.com>
2017-06-04 22:56:04 +0530

    Complete project

--- src/test/java/com/selenium/example/stepDefinition.java
+++ src/test/java/com/selenium/example/stepDefinition.java
@@ -9,9 +9,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -25,20 +29,27 @@ public class stepDefinition {
 	WebElement enter=null;
 	String OS = System.getProperty("os.name").toLowerCase();
 	
-	@Given("^I navigate to \"([^\"]*)\"$")
+	@Given("^I navigate to \"([^\"]*)\" in firefox$")
 	public void i_navigate_to(String url) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 		if(OS.contains("windows")){
 			System.setProperty("webdriver.gecko.driver", "src/test/resources/com/selenium/example/windows/geckodriver.exe");
 		}else if(OS.contains("linux")){
 			System.setProperty("webdriver.gecko.driver", "src/test/resources/com/selenium/example/linux/geckodriver");
 		}
 		allProfiles =new ProfilesIni();
-		profile = allProfiles.getProfile("skipssl");
+		profile = allProfiles.getProfile("skipssl");		
 		driver = new FirefoxDriver(profile); 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(url);
 	}
+	@Given("^I navigate to \"([^\"]*)\" in headless browser$")
+	public void i_navigate_to_in_headless_browser(String url) throws Throwable {
+		HtmlUnitDriver driver = new HtmlUnitDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(url);
+	    throw new PendingException();
+	}
+
 	
 	@Given("^I enter \"([^\"]*)\" into input field having id \"([^\"]*)\"$")
 	public void i_enter_into_input_field_having_id(String fname, String fname_id) throws Throwable {
@@ -47,19 +58,30 @@ public class stepDefinition {
 	}
 	@When("^I press enter$")
 	public void i_press_enter() throws Throwable {	  
-	    enter = driver.findElement(By.xpath("//*[@id='registerForm']/div[8]/button"));
+	    enter = driver.findElement(By.xpath("//*[@id='registerForm']/div[11]/button"));
 	    enter.sendKeys(Keys.RETURN);
 	    //throw new PendingException();
+	}
+	@When("^I press Login$")
+	public void i_press_Login() throws Throwable {
+		System.out.println("hi");
+		 enter = driver.findElement(By.xpath("//*[@id='loginForm']/button"));
+		 enter.sendKeys(Keys.RETURN);
+	    throw new PendingException();
 	}
 	@When("^I wait for (\\d+) sec$")
 	public void i_wait_for_sec(int sec) throws Throwable {
 	    Thread.sleep(sec*1000);
 	    //throw new PendingException();
 	}
+	@When("^take screenshot with filename \"([^\"]*)\"$")
+	public void take_screenshot_with_filename(String fileName) throws Throwable {
+		 File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	     FileUtils.copyFile(scrFile, new File("src/Screenshot/"+fileName));
+	    throw new PendingException();
+	}
 	@Then("^I close browser$")
 	public void i_close_browser() throws Throwable {
 	    driver.quit();
-	    //throw new PendingException();
 	}
-
-     }
+ }
--- src/test/resources/com/selenium/example/sample.feature
+++ src/test/resources/com/selenium/example/sample.feature
@@ -2,8 +2,8 @@
 Feature: To test my cucumber test is running
 I want to run a sample feature file.
 
- Scenario: I login with valid credential
-       Given I navigate to "https://34.200.9.250:9002/PE/es_PE/login"
+ Scenario: I register with valid credential
+       Given I navigate to "https://34.200.9.250:9002/PE/es_PE/login" in firefox
        And I enter "Guido" into input field having id "register.firstName"
        And I enter "Maiola" into input field having id "register.lastName"
        And I enter "guidomaiola@gmail.com.com" into input field having id "register.email"
@@ -11,4 +11,14 @@ I want to run a sample feature file.
        And I enter "Pass1234" into input field having id "register.checkPwd"
        When I press enter
        And I wait for 15 sec
+	   And take screenshot with filename "screenshot1.png" 
+       Then I close browser
+ 
+  Scenario: I login with valid credential
+       Given I navigate to "https://34.200.9.250:9002/PE/es_PE/login" in headless browser
+       And I enter "Guido" into input field having id "j_username"
+       And I enter "Maiola" into input field having id "j_password"
+       When I press Login
+       And I wait for 15 sec
+	   And take screenshot with filename "screenshot2.png" 
        Then I close browser
